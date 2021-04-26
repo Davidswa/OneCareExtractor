bbf48ece68817b2865133dc1fe94291fd68933d3
Davidswa <David.Swartzen@gmail.com>
2021-04-26 11:21:39 -0700

    Changed to CLI, tested and works

--- File_Aggregator.py
+++ File_Aggregator.py
@@ -9,39 +9,67 @@
 import os
 import pathlib
 from shutil import copyfile
+import argparse as ap
 
-ROOT_PATH = "I:\\180541\\BBBB"
-RECV_FOLDER_NAME = "Reconstructed Dir"
-RECV_FULL = ROOT_PATH + "\\" + RECV_FOLDER_NAME
+parser = ap.ArgumentParser(description='Reconstruct OneCare backups.')
+
+parser.add_argument('src', 
+                    type=str, 
+                    help='Full path to OneCare backup of interest')
+
+parser.add_argument('dst', 
+                    type=str, 
+                    help='Full path to store unpacked backup info')
+
+args = parser.parse_args()
+
+ROOT_PATH = args.src
+RECV_FULL = args.dst
 
 file_list = []
 dir_list =[]
 
+print("Source: %s" % args.src)
+print("Destination: %s" % args.dst)
+
+
 # Walk folders to find all files and paths, store in list
-for paths, dirs, files in os.walk(ROOT_PATH):
-    for item in files:
-        file_list.append(os.path.join(paths,item))
-    for item in dirs:
-        p = pathlib.Path(os.path.relpath(os.path.join(paths,item),ROOT_PATH))
-        dir_list.append(pathlib.Path(*p.parts[1:]))
+def create_lists():
+    for paths, dirs, files in os.walk(ROOT_PATH):
+        for item in files:
+            file_list.append(os.path.join(paths,item))
+        for item in dirs:
+            p = pathlib.Path(os.path.relpath(os.path.join(paths,item),ROOT_PATH))
+            dir_list.append(pathlib.Path(*p.parts[1:]))
+    return [dir_list,file_list]
 
 # Create folder structure to place recovered items in
-if not os.path.exists(RECV_FULL):
-    os.mkdir(RECV_FULL)
-else:
-    print("Recovery dir already made!")
+def create_structure(dir_in):
+    if not os.path.exists(RECV_FULL):
+        os.mkdir(RECV_FULL)
+    else:
+        print("Recovery dir already made!")
 
-for item in dir_list:
-    try:
-        os.makedirs(os.path.join(RECV_FULL,item), exist_ok=True)
-    except OSError as e:
-        print("Directory `%s` can not be created" % item)
+    for item in dir_in:
+        try:
+            os.makedirs(os.path.join(RECV_FULL,item), exist_ok=True)
+        except OSError as e:
+            print("Directory `%s` can not be created" % item)
 
 # Move files to new folder structure
-for item in file_list:
-    p = pathlib.Path(item)
-    a = RECV_FULL + "\\" + str(pathlib.Path(*p.parts[4:]))
-    copyfile(item,a)
+def move_files(file_in):
+    for item in file_in:
+        p = pathlib.Path(item)
+        a = RECV_FULL + "\\" + str(pathlib.Path(*p.parts[4:]))
+        copyfile(item,a)
 
+def main():
+    [dir_out,file_out] = create_lists()
+    print("Lists created...")
+    create_structure(dir_out)
+    print("Structure rebuild completed...")
+    move_files(file_out)
+    print("\n\n=============================\nScript completed!\n=============================\n")
 
-print("\n\n=============================\nScript completed!\n=============================\n")
+if __name__ == "__main__":
+    main()
